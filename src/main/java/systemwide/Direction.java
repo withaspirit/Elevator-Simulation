287ff7c154847577a800c276e9e1d307d0732dae
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-01-25 15:16:55 -0500

    Modified Direction.java, added getter for Direction as a string

--- src/main/java/systemwide/Direction.java
+++ src/main/java/systemwide/Direction.java
@@ -1,5 +1,8 @@
 package systemwide;
 
+import java.util.HashMap;
+import java.util.Locale;
+
 /**
  * Direction indicates vertical direction in terms of up, down, and stopped.
  * 
@@ -7,5 +10,43 @@ package systemwide;
  *
  */
 public enum Direction {
-	UP, DOWN, STOPPED;
+	UP("Up"),
+	DOWN("Down"),
+	STOPPED("Stop");
+
+	private String name;
+
+	/**
+	 * The constructor for Direction.
+	 *
+	 * @param name the name of the Direction
+	 */
+	Direction(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Returns the name of the Direction.
+	 *
+	 * @return name the name of the Direction
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns the Direction with the specified name.
+	 * @param name the name of the direction
+	 *
+	 * @return the Direction with the specified name
+	 */
+	public static Direction getDirection(String name) {
+		try {
+			return valueOf(name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase());
+		} catch (IllegalArgumentException iae) {
+			System.out.println("Direction does not exist");
+			iae.printStackTrace();
+			return null;
+		}
+	}
 }
