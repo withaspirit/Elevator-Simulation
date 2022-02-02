33fbc28f84dc6681ae8ef3b4f21e548c54e8f49d
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-02-02 18:59:08 -0500

    Added Method for creating FloorRequests from ElevatorRequests

--- src/main/java/misc/RequestFactory.java
+++ src/main/java/misc/RequestFactory.java
@@ -5,7 +5,7 @@ import systemwide.Direction;
 import java.time.LocalTime;
 
 /**
- * RequestFactory is a factory for creating requests.
+ * RequestFactory is a factory for creating ServiceRequests.
  *
  * @author Liam Tripp
  */
@@ -15,7 +15,7 @@ public class RequestFactory {
      * Creates an ElevatorRequest from a given String array.
      *
      * @param data a String array containing information for the ElevatorRequest
-     * @return elevatorRequest and
+     * @return elevatorRequest a request for an elevator made from an input file
      */
     public ElevatorRequest createElevatorRequest(String[] data) {
         LocalTime time = LocalTime.parse(data[0]);
@@ -24,5 +24,17 @@ public class RequestFactory {
         int floorToVisit = Integer.parseInt(data[3]);
 
         return new ElevatorRequest(time, floorNumber, direction, floorToVisit);
+    }
+
+    /**
+     * Creates a FloorRequest given an ElevatorRequest and an Elevator's Number.
+     *
+     * @param elevatorRequest a ServiceRequest for an Elevator made by someone on a Floor
+     * @param elevatorNumber the number of the elevator
+     * @return floorRequest a request for an elevator to visit a floor made by an input file
+     */
+    public FloorRequest createFloorRequest(ElevatorRequest elevatorRequest, int elevatorNumber) {
+        return new FloorRequest(elevatorRequest.getTime(), elevatorRequest.getDesiredFloor(),
+                elevatorRequest.getDirection(), elevatorNumber);
     }
 }
