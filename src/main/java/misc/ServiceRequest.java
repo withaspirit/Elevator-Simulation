7d15ba69ddec262836f3a7e66123a4b782a1e52a
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-02-03 16:19:37 -0500

    Update src/main/java/misc/ServiceRequest.java
    Co-authored-by: ryandash <56605453+ryandash@users.noreply.github.com>

--- src/main/java/misc/ServiceRequest.java
+++ src/main/java/misc/ServiceRequest.java
@@ -12,7 +12,7 @@ import java.time.LocalTime;
  */
 //FIXME: This could be refactored into an abstract class, but is an interface for now due to design uncertainty
 public interface ServiceRequest {
-	int getTime();
+	LocalTime getTime();
 	int getFloorNumber();
 	Direction getDirection();
 }
