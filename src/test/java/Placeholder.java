10d6448279f1f6c7a0528ad3192b901cf73a95a9
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-02-03 12:06:56 -0500

    Bugfix for Direction.getDirection(String) not working, adding JUNIT Tests for Direction

--- src/test/java/Placeholder.java
+++ src/test/java/Placeholder.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import systemwide.Direction;
+
+/**
+ * DirectionTest ensures that Direction's search method functions correctly.
+ *
+ * @author Liam Tripp
+ */
 public class Placeholder {
+
+    @Test
+    public void testGetDirection() {
+        for (Direction direction : Direction.values()) {
+            String directionName = direction.getName();
+            Direction foundDirection = Direction.getDirection(directionName);
+            Assertions.assertNotNull(foundDirection);
+            Assertions.assertEquals(directionName, foundDirection.getName());
+        }
+    }
+
+    @Test
+    public void testGetDirectionWithLowerCaseAndWhiteSpace() {
+        for (Direction direction : Direction.values()) {
+            String directionName = " " + direction.getName().toLowerCase() + " ";
+            Direction foundDirection = Direction.getDirection(directionName);
+            Assertions.assertNotNull(foundDirection);
+        }
+    }
 }
