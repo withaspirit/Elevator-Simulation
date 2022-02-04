9d7aec3cec8d8f75bc562131feeaf2e44a87b629
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-02-03 22:45:59 -0500

    Request abstraction (#22)
    * Added Inheritance for ServiceRequests -> (ElevatorRequests, FloorRequests)
    
    * Fixed ElevatorRequest.toString() issues

--- src/main/java/misc/ElevatorRequest.java
+++ src/main/java/misc/ElevatorRequest.java
@@ -9,22 +9,12 @@ import java.time.format.DateTimeFormatter;
  * ElevatorRequest is an event data structure for when a user presses an up/down
  * button on a Floor.
  *
- * @author Liam Tripp
+ * @author Liam Tripp, Ramit Mahajan
  */
-public class ElevatorRequest implements ServiceRequest {
+public class ElevatorRequest extends ServiceRequest {
 
-	private LocalTime time;
-	private int floorNumber;
-	private Direction direction;
 	private int desiredFloor;
-	private String basicAction;
-
-	public ElevatorRequest(LocalTime time, int floorNumber, Direction direction) {
-		this.time = time;
-		this.floorNumber = floorNumber;
-		this.direction = direction;
-		this.desiredFloor = -1; // Error Value
-	}
+//	private String basicAction;
 
 	/**
 	 * Constructor for ElevatorRequest read from an input file.
@@ -35,33 +25,30 @@ public class ElevatorRequest implements ServiceRequest {
 	 * @param desiredFloor the floor the user wishes to visit
 	 */
 	public ElevatorRequest(LocalTime time, int floorNumber, Direction direction, int desiredFloor) {
-		this(time, floorNumber, direction);
+		super(time ,floorNumber, direction);
 		this.desiredFloor = desiredFloor;
 	}
 
-	//TODO move to a different class
-	public ElevatorRequest(String basicAction) {
-		this.basicAction = basicAction;
+	/**
+	 * Constructor for ElevatorRequest read from an input file.
+	 *
+	 * @param time the time the Request was made
+	 * @param floorNumber the number of the floor on which the request was made
+	 * @param direction the direction selected by the user
+	 */
+	public ElevatorRequest(LocalTime time, int floorNumber, Direction direction) {
+		super(time, floorNumber, direction);
+		this.desiredFloor = -1; // Error Value
 	}
+
+//	//TODO move to a different class
+//	public ElevatorRequest(String basicAction) {
+//		this.basicAction = basicAction;
+//	}
 
 	// do not use if created from floorRequest
 	public int getDesiredFloor() {
 		return desiredFloor;
-	}
-
-	@Override
-	public LocalTime getTime() {
-		return time;
-	}
-
-	@Override
-	public int getFloorNumber() {
-		return floorNumber;
-	}
-
-	@Override
-	public Direction getDirection() {
-		return direction;
 	}
 
 	/**
@@ -75,8 +62,8 @@ public class ElevatorRequest implements ServiceRequest {
 	@Override
 	public String toString() {
 		DateTimeFormatter dateTimeFormat = DateTimeFormatter.ofPattern("HH:mm:ss.SSS");
-		String formattedDate = time.format(dateTimeFormat);
-		String formattedString = formattedDate + " " + floorNumber + " " + direction.getName();
+		String formattedDate = getTime().format(dateTimeFormat);
+		String formattedString = formattedDate + " " + getFloorNumber() + " " + getDirection().getName();
 
 		if (desiredFloor != -1) {
 			formattedString += " " + desiredFloor;
