6876877ef9e6549b165c93581d7a57e7a5b07976
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-02-05 12:58:12 -0500

    Implementing interface with default methods sendMessage, receiveMessage in Scheduler for passing objects

--- src/main/java/misc/ServiceRequestListener.java
+++ src/main/java/misc/ServiceRequestListener.java
@@ -2,10 +2,43 @@ package misc;
 
 /**
  * ServiceRequestListener allows ____ and ____ to respond to ServiceRequests.
- * 
- * @author Liam Tripp
+ *
+ * @author Liam Tripp, Ryan Dash
  */
 public interface ServiceRequestListener {
-	public void handleElevatorRequest(ElevatorRequest elevatorRequest);
-	public void handleFloorRequest(FloorRequest floorRequest);
+
+	/**
+	 * Adds an object to a buffer.
+	 *
+	 * @param object an arbitrary object (a Request for this implementation)
+	 * @param buffer a BoundedBuffer which holds the object
+	 * @return true if request is successful, false otherwise
+	 */
+	default boolean sendMessage(Object object, BoundedBuffer buffer) {
+		System.out.println(Thread.currentThread().getName() + " sending: " + object);
+		buffer.addLast((ServiceRequest) object);
+
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+		}
+		return true;
+	}
+
+	/**
+	 * Removes and returns an object from a buffer.
+	 *
+	 * @param buffer a buffer which holds object
+	 * @return object the first object in the buffer
+	 */
+	default Object receiveMessage(BoundedBuffer buffer) {
+		Object object = buffer.removeFirst();
+		System.out.println(Thread.currentThread().getName() + " received: " + object);
+
+		try {
+			Thread.sleep(0);
+		} catch (InterruptedException e) {
+		}
+		return object;
+	}
 }
