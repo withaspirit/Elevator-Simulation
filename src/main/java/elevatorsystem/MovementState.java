77ce66701280b2376285528b477a774e8380aec4
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-02-09 16:24:56 -0500

    Reduced number of movement states
    Signed-off-by: cyberphoria <61635007+cyberphoria@users.noreply.github.com>

---
+++ src/main/java/elevatorsystem/MovementState.java
@@ -0,0 +1,12 @@
+package elevatorsystem;
+
+/**
+ * MovementState indicates the movement state of the elevator.
+ *
+ * @author Liam Tripp
+ */
+public enum MovementState {
+    IDLE,
+    MOVING,
+    STUCK;
+}
--- src/main/java/systemwide/MovementState.java
+++
@@ -1,9 +0,0 @@
-package systemwide;
-
-public enum MovementState {
-    STOP,
-    CONSTANT_SPEED,
-    ACCELERATE,
-    DECELERATE,
-    STUCK;
-}
