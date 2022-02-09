47a38956b54cab32431a97c259c03e7ceb24186f
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-02-09 13:31:26 -0500

    Add setters, getters for Structure
    Signed-off-by: cyberphoria <61635007+cyberphoria@users.noreply.github.com>

--- src/main/java/systemwide/Structure.java
+++ src/main/java/systemwide/Structure.java
@@ -12,17 +12,53 @@ import misc.BoundedBuffer;
  */
 public class Structure {
 	
-	private final int numberOfFloors;
-	private final int numberOfElevators;
+	private int numberOfFloors;
+	private int numberOfElevators;
 
 	/**
 	 * Constructor for Structure.
 	 *
 	 * @param numberOfFloors the number of floors in the structure
-	 * @param numberOfElevators
+	 * @param numberOfElevators the number of elevators in the structure
 	 */
 	public Structure(int numberOfFloors, int numberOfElevators) {
 		this.numberOfFloors = numberOfFloors;
+		this.numberOfElevators = numberOfElevators;
+	}
+
+	/**
+	 * Returns the number of floors in the structure.
+	 *
+	 * @return the number of floors
+	 */
+	public int getNumberOfFloors() {
+		return numberOfFloors;
+	}
+
+	/**
+	 * Sets the number of floors in the structure.
+	 *
+	 * @param numberOfFloors number of floors
+	 */
+	public void setNumberOfFloors(int numberOfFloors) {
+		this.numberOfFloors = numberOfFloors;
+	}
+
+	/**
+	 * Returns the number of elevators in the structure.
+	 *
+	 * @return the number of elevators
+	 */
+	public int getNumberOfElevators() {
+		return numberOfElevators;
+	}
+
+	/**
+	 * Returns the number of elevators in the structure.
+	 *
+	 * @param numberOfElevators number of elevators
+	 */
+	public void setNumberOfElevators(int numberOfElevators) {
 		this.numberOfElevators = numberOfElevators;
 	}
 
