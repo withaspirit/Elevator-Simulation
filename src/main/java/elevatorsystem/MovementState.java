a32063579be66d95b3c43b35727ee64b45b40447
Brady Norton <bnort.11@gmail.com>
2022-02-16 04:52:27 -0500

    Elevator Motion:
    - Basic implementation of the elevator properties
    - Majority of getters/setters implemented
    - ACCELERATION, MAX_SPEED, and LOAD_TIME now should be much more accurate than their original values
    - Missing: Method for determining total time it will take to reach destination using our previously mentioned values
    
    Signed-off-by: Brady Norton <bnort.11@gmail.com>

--- src/main/java/elevatorsystem/MovementState.java
+++ src/main/java/elevatorsystem/MovementState.java
@@ -3,10 +3,40 @@ package elevatorsystem;
 /**
  * MovementState indicates the state of movement activity of the elevator.
  *
- * @author Liam Tripp
+ * @author Liam Tripp, Brady Norton
  */
 public enum MovementState {
-    IDLE,
-    ACTIVE,
-    STUCK;
+    IDLE("IDLE"),
+    ACTIVE("ACTIVE"),
+    STUCK("STUCK");
+
+    private String name;
+
+
+    /**
+     * Constructor for MovementState class
+     * @param name the name of the MovementState
+     */
+    MovementState(String name){ this.name = name;}
+
+
+    /**
+     * Gets the name of the MovementState as a String
+     *
+     * @return name of MovementState
+     */
+    public String getName(){ return name; }
+
+
+    /**
+     * Returns the MovementState with the specified name
+     *
+     * @param s the name of the MovementState as a String
+     * @return the MovementState and it's name
+     */
+    public static MovementState getState(String s) {
+        if ("IDLE".equals(s)) {return IDLE;}
+        else if ("ACTIVE".equals(s)) {return ACTIVE;}
+        else {return STUCK;}
+    }
 }
