7fab145e8f571a3247b6d29c279a094147b27df7
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-02-17 15:39:31 -0500

    Added ApproachEvent passing to Floor, FloorSubsystem

--- src/main/java/floorsystem/Floor.java
+++ src/main/java/floorsystem/Floor.java
@@ -1,5 +1,7 @@
 package floorsystem;
 
+import requests.ApproachEvent;
+
 /**
  * Floor simulates a level of a structure that an elevator can visit.
  * 
@@ -26,4 +28,13 @@ public class Floor {
 	 * buttons.forEach(button -> button.addActionListener(...));
 	 *
 	 */
+
+	/**
+	 * Passes an ApproachEvent to the ElevatorSubsystem.
+	 *
+	 * @param approachEvent the ApproachEvent that is examined by the ArrivalSensor
+	 */
+	public void passApproachEvent(ApproachEvent approachEvent) {
+		floorSubsystem.handleApproachEvent(approachEvent);
+	}
 }
