3709a108bd8e797f5769053067a203b2d4d6075b
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-02-28 10:03:12 -0500

    Added JUnit test for Floor.receiveApproachEvent()

---
+++ src/test/java/floorsystem/FloorTest.java
@@ -0,0 +1,48 @@
+package floorsystem;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import requests.ApproachEvent;
+import systemwide.BoundedBuffer;
+import systemwide.Direction;
+import systemwide.Origin;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+/**
+ * FloorTest verifies the methods of the Floor class. Namely, the
+ * modification of an ApproachEvent's properties.
+ *
+ * @author Liam Tripp
+ */
+public class FloorTest {
+
+    private Floor floor;
+    private ApproachEvent approachEvent;
+
+    @BeforeEach
+    void setUp() {
+        BoundedBuffer buffer = new BoundedBuffer();
+        FloorSubsystem floorSubsystem = new FloorSubsystem(buffer);
+        floor = new Floor(1, floorSubsystem);
+    }
+
+    @Test
+    void testApproachEventNotChanged() {
+        int floorNumber = 2;
+        approachEvent = new ApproachEvent(LocalTime.now(), floorNumber, Direction.UP, 1, Origin.FLOOR_SYSTEM);
+        floor.receiveApproachEvent(approachEvent);
+        assertFalse(approachEvent.elevatorMayStop());
+    }
+
+    @Test
+    void testApproachEventChanged() {
+        int floorNumber = 1;
+        approachEvent = new ApproachEvent(LocalTime.now(), floorNumber, Direction.UP, 1, Origin.FLOOR_SYSTEM);
+        floor.receiveApproachEvent(approachEvent);
+        assertTrue(approachEvent.elevatorMayStop());
+    }
+ }
