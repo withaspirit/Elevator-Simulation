f7454c9616f8f97b7e37737457dc316370e57ae4
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-03-02 18:53:49 -0500

    Added test to PriorityQueueTest to demonstrate compatibility with PriorityQueue

--- src/test/java/requests/PriorityQueueTest.java
+++ src/test/java/requests/PriorityQueueTest.java
@@ -14,8 +14,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * PriorityQueueTest demonstrates the utility of PriorityQueue with Comparators
  * and Comparable classes. PriorityBlockingQueue is used due to concurrency issues.
+ * Note that the toString() method for PriorityQueue doesn't print the elements
+ * as they appear in the list.
  *
- * resources used:
+ * Resources used:
  * https://howtodoinjava.com/java/collections/java-priorityqueue/
  * https://stackoverflow.com/a/10078729
  *
@@ -23,13 +25,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class PriorityQueueTest {
 
+    private ArrayList<ServiceRequest> requests;
     private PriorityBlockingQueue<ServiceRequest> forwardQueue;
     private PriorityBlockingQueue<ServiceRequest> backwardsQueue;
     private final int NUM_REQUESTS = 9;
 
     @BeforeEach
     void setUp() {
-        ArrayList<ServiceRequest> requests = new ArrayList<>();
+        requests = new ArrayList<>();
         for (int i = 1; i <= NUM_REQUESTS; i++) {
             requests.add(new ServiceRequest(LocalTime.now(), i, Direction.DOWN, Origin.ELEVATOR_SYSTEM));
         }
@@ -44,17 +47,35 @@ public class PriorityQueueTest {
         backwardsQueue.addAll(requests);
     }
 
+    /**
+     * Add ElevatorRequests to requests. Demonstrates compatibility with Inheritance.
+     */
+    void addElevatorRequests() {
+        for (int i = 10; i <= NUM_REQUESTS + 10; i++) {
+            if (i % 2 == 0) {
+                requests.add(new ElevatorRequest(LocalTime.now(), i, Direction.UP, i + 1, Origin.ELEVATOR_SYSTEM));
+            }
+        }
+    }
+
     @Test
     void testForwardQueueIsInOrder() {
-        for (int i = 1; i <= NUM_REQUESTS; i++) {
+        for (int i = 1; i <= forwardQueue.size(); i++) {
             assertEquals(i, forwardQueue.remove().getFloorNumber());
         }
     }
 
     @Test
     void testBackwardQueueIsInOrder() {
-        for (int i = NUM_REQUESTS - 1; i >= 0; i--) {
+        for (int i = backwardsQueue.size() - 1; i >= 0; i--) {
             assertEquals(i + 1, backwardsQueue.remove().getFloorNumber());
         }
+    }
+
+    @Test
+    void testWithElevatorRequests() {
+        addElevatorRequests();
+        testBackwardQueueIsInOrder();
+        testForwardQueueIsInOrder();
     }
 }
