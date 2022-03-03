1417abb66b9738ff728b588d9f475c4f75457e0a
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-03-03 12:05:30 -0500

    FloorsQueue: added currentDirectionQueue to keep track of which queue is currently being used.

--- src/main/java/elevatorsystem/FloorsQueue.java
+++ src/main/java/elevatorsystem/FloorsQueue.java
@@ -1,6 +1,7 @@
 package elevatorsystem;
 
 import java.util.*;
+
 import systemwide.Direction;
 
 /**
@@ -13,6 +14,8 @@ public class FloorsQueue {
 	private PriorityQueue<Integer> upwardRequests;
 	private PriorityQueue<Integer> downwardRequests;
 	private Queue<Integer> missedRequests;
+	private volatile PriorityQueue<Integer> currentDirectionQueue;
+
 
 	/**
 	 * Constructor for the class
@@ -21,6 +24,7 @@ public class FloorsQueue {
 		this.upwardRequests = new PriorityQueue<>();
 		this.downwardRequests = new PriorityQueue<>(Collections.reverseOrder());
 		this.missedRequests = new LinkedList<>();
+		currentDirectionQueue = upwardRequests;
 	}
 
 	/**
@@ -60,24 +64,22 @@ public class FloorsQueue {
 	/**
 	 * Removes the next floor to flag that the floor has been visited
 	 * 
-	 * @param direction the direction the elevator came to the floor
+	 * @param serviceDirection the serviceDirection the elevator came to the floor
 	 * @return floorVisited the floor that has been visited, -1 if not successful
 	 */
-	public int visitNextFloor(Direction direction) {
+	public int visitNextFloor(Direction serviceDirection) {
 		int floorVisited = -1;
 
-		if (direction == Direction.UP) {
+		if (serviceDirection == Direction.UP) {
 			if (!upwardRequests.isEmpty()) {
 				floorVisited = upwardRequests.remove();
 				if (upwardRequests.isEmpty()) { // It automatically swaps if the upward queue is emptied
-					swapQueues(direction);
 				}
 			}
-		} else if (direction == Direction.DOWN) {
+		} else if (serviceDirection == Direction.DOWN) {
 			if (!downwardRequests.isEmpty()) {
 				floorVisited = downwardRequests.remove();
 				if (downwardRequests.isEmpty()) { // It automatically swaps if the downward queue is emptied
-					swapQueues(direction);
 				}
 			}
 		} else {
@@ -93,7 +95,15 @@ public class FloorsQueue {
 	 * @return nextFloor the next floor in queue, -1 if not successful
 	 */
 	public int peekNextFloor(Direction direction) {
-		int nextFloor = -1;
+		if (!currentDirectionQueue.isEmpty()) {
+			return currentDirectionQueue.peek();
+		} else {
+			System.err.println("FloorQueue.peekNextFloor should not be accessed " +
+					"while the active queue is empty. Swapping should be done beforehand.");
+			return -1;
+		}
+
+		/*
 		if (direction == Direction.UP) {
 			if (!upwardRequests.isEmpty()) {
 				nextFloor = upwardRequests.peek();
@@ -105,17 +115,41 @@ public class FloorsQueue {
 		} else {
 			throw new RuntimeException("Direction is invalid");
 		}
-		return nextFloor;
+		 */
 	}
 
 	/**
 	 * Updates / Swaps the queues to include the missed requests
 	 * 
-	 * @param direction the direction of the queue wanting to swap
+	 * @param serviceDirection the direction of the queue wanting to swap
 	 * @return status 0 if successful, -1 if not successful
 	 */
-	public int swapQueues(Direction direction) {
+	public int swapQueues(Direction serviceDirection) {
 		int status = -1;
+
+		if (currentDirectionQueue.isEmpty()) {
+			// add any missed requests to current queue
+			while (!missedRequests.isEmpty()) {
+				currentDirectionQueue.add(missedRequests.remove());
+			}
+
+			// switch to opposite direction queue if possible
+			if (serviceDirection == Direction.UP) {
+				if (!downwardRequests.isEmpty()) {
+					currentDirectionQueue = downwardRequests;
+					status = 0;
+				}
+			} else if (serviceDirection == Direction.DOWN) {
+				if (!upwardRequests.isEmpty()) {
+					currentDirectionQueue = upwardRequests;
+					status = 0;
+				}
+			} else {
+				throw new RuntimeException("Direction is invalid");
+			}
+		}
+
+		/*
 		if (direction == Direction.UP && upwardRequests.isEmpty()) {
 			while (!missedRequests.isEmpty()) {
 				upwardRequests.add(missedRequests.remove());
@@ -127,6 +161,7 @@ public class FloorsQueue {
 			}
 			status = 0;
 		}
+		 */
 		return status;
 	}
 
