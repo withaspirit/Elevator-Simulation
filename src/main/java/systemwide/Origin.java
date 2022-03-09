c8b93eb1a54b7f656504ad97007a12b6ada50cf2
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-03-09 17:50:38 -0500

    Origin: changeOrigin(origin) to change Origin to opposite direction.

--- src/main/java/systemwide/Origin.java
+++ src/main/java/systemwide/Origin.java
@@ -9,4 +9,20 @@ public enum Origin {
     FLOOR_SYSTEM,
     ELEVATOR_SYSTEM,
     SCHEDULER;
+
+    /**
+     * Provides an Origin in the opposite direction of the Origin provided.
+     *
+     * @param origin the Origin provided
+     * @return an Origin in the opposite direction of the Origin provided.
+     */
+    public static Origin changeOrigin(Origin origin) {
+        if (origin == FLOOR_SYSTEM) {
+            return ELEVATOR_SYSTEM;
+        } else if (origin == ELEVATOR_SYSTEM){
+            return FLOOR_SYSTEM;
+        } else {
+            throw new IllegalArgumentException("Error: Trying to change origin when origin is Scheduler.");
+        }
+    }
 }
