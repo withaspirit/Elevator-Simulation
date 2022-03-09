63b4546f04694188701fdb5683c929fdd432c4ff
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-03-09 11:26:26 -0500

    Add Client, Server, IntermediateHost, Port, MessageTransfer (#115)
    - Added Client, Server, and IntermediateHost classes for transferring messages 
    - Added Port: enum for the port numbers of one clients, two hosts, and one server
    - Added MessageTransfer: class with DatagramSocket for Transferring DatagramPackets
    - MessageTransfer has methods for transferring and printing DatagramPackets
    - Client, Server, IntermediateHost have MessageTransfer objects to enable them to transfer messages

---
+++ src/main/java/client_server_host/IntermediateHost.java
@@ -0,0 +1,28 @@
+package client_server_host;
+
+import java.net.DatagramPacket;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * IntermediateHost receives and sends message from both Client and Server.
+ *
+ * @author Liam Tripp
+ */
+public class IntermediateHost {
+
+    private int portNumber;
+    private Queue<DatagramPacket> messageQueue;
+    private MessageTransfer messageTransfer;
+
+    /**
+     * Constructor for IntermediateHost.
+     *
+     * @param portNumber the port number corresponding to the IntermediateHost thread
+     */
+    public IntermediateHost(int portNumber) {
+        this.portNumber = portNumber;
+        messageQueue = new LinkedList<>();
+        messageTransfer = new MessageTransfer(portNumber);
+    }
+}
