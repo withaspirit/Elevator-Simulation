4bc953dab50dd5d1930cfeafe6ef7f40eeff25c3
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-03-10 00:57:59 -0500

    IntermediateHost: remove queue

--- src/main/java/client_server_host/IntermediateHost.java
+++ src/main/java/client_server_host/IntermediateHost.java
@@ -1,8 +1,6 @@
 package client_server_host;
 
 import java.net.DatagramPacket;
-import java.util.LinkedList;
-import java.util.Queue;
 
 /**
  * IntermediateHost receives and sends message from both Client and Server.
@@ -11,8 +9,6 @@ import java.util.Queue;
  */
 public class IntermediateHost {
 
-    private int portNumber;
-    private Queue<DatagramPacket> messageQueue;
     private MessageTransfer messageTransfer;
 
     /**
@@ -21,8 +17,6 @@ public class IntermediateHost {
      * @param portNumber the port number corresponding to the IntermediateHost thread
      */
     public IntermediateHost(int portNumber) {
-        this.portNumber = portNumber;
-        messageQueue = new LinkedList<>();
         messageTransfer = new MessageTransfer(portNumber);
     }
 }
