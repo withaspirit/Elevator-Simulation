ec4edbc3c7f3725cdd0293539d5ae560d03bb52d
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-03-10 21:27:45 -0500

    ElevatorMotor: corrected method description

--- src/main/java/elevatorsystem/ElevatorMotor.java
+++ src/main/java/elevatorsystem/ElevatorMotor.java
@@ -75,7 +75,8 @@ public class ElevatorMotor {
 	}
 
 	/**
-	 * Changes the direction of the Motor depending on the direction of the elevator.
+	 * Changes the direction of the Motor depending on the elevator's location
+	 * and the location of its next floor to visit.
 	 *
 	 * @param currentFloor the current floor of the elevator
 	 * @param requestFloor the number of the elevator's next floor to visit
