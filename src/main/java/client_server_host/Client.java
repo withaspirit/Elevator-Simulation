3c1ca32ddd74ce710db5d0257ea3c36b564a1bd1
Julian <julianobandovelez@cmail.carleton.ca>
2022-03-10 23:42:01 -0500

    Included methods for client to process packets

--- src/main/java/client_server_host/Client.java
+++ src/main/java/client_server_host/Client.java
@@ -1,5 +1,11 @@
 package client_server_host;
 
+import requests.SystemEvent;
+
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Client sends and receives messages from an IntermediateHost.
  *
@@ -8,11 +14,48 @@ package client_server_host;
 public class Client {
 
     private MessageTransfer messageTransfer;
+    private InetAddress inetAddress;
 
     /**
      * Constructor for Client.
      */
     public Client() {
         messageTransfer = new MessageTransfer(Port.CLIENT.getNumber());
+        try {
+			inetAddress = InetAddress.getLocalHost();
+		}  catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
     }
+    
+    public void sendPacket(DatagramPacket sendPacket) {
+    	messageTransfer.sendMessage(sendPacket);
+    }
+    
+    public DatagramPacket buildPacket(Object object) {
+    	byte[] newByteArray;
+    	
+        if (object instanceof SystemEvent) {
+        	newByteArray = messageTransfer.encodeObject(object);
+        } else if (object instanceof String) {
+        	newByteArray = ((String)object).getBytes();
+        } else {
+        	throw new IllegalArgumentException("Error: Invalid Object");
+        }
+        DatagramPacket newPacket = messageTransfer.createPacket(newByteArray, Port.CLIENT_TO_SERVER.getNumber());
+        return newPacket;
+    }
+    
+    /**
+     * Receives and returns a DatagramPacket from the IntermediateHost's MessageTransfer.
+     *
+     * @return packet received from the IntermediateHost's MessageTransfer
+     */
+    public DatagramPacket receivePacket() {
+        DatagramPacket receivePacket = messageTransfer.createEmptyPacket();
+        messageTransfer.receiveMessage(receivePacket);
+        return receivePacket;
+    }
+    
+
 }
