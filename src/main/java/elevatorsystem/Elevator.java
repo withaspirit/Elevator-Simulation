6367e72875d05fe868d9badecb30fd4a63eba139
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-03-19 16:32:58 -0400

    Elevator: remove statement causing Concurrency error

--- src/main/java/elevatorsystem/Elevator.java
+++ src/main/java/elevatorsystem/Elevator.java
@@ -147,7 +147,8 @@ public class Elevator implements Runnable, SubsystemPasser {
 		if (removedFloor == -1) {
 			throw new IllegalArgumentException("A value of -1 was received from the requestQueue.");
 		} else if (!sameFloorRemovedAsPeeked) {
-			throw new ConcurrentModificationException("A request was added while the current request was being processed.");
+			System.out.println("Floor peeked " + requestFloor + ", Floor Removed: " + removedFloor);
+			throw new ConcurrentModificationException("A request was added to Elevator " + elevatorNumber + " while the current request was being processed.");
 		}
 	}
 
@@ -247,7 +248,6 @@ public class Elevator implements Runnable, SubsystemPasser {
 	 */
 	public void addRequest(ServiceRequest serviceRequest) {
 		//TODO remove after queueTime updated properly and serviceDirection is updated properly
-		motor.setMovementState(MovementState.ACTIVE);
 		if (serviceRequest instanceof ElevatorRequest elevatorRequest) {
 			queueTime = getExpectedTime(elevatorRequest);
 		}
