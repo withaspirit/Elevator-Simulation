2c2b752fbd1d5ded1b191b357df29b280cc20a4d
ryandash <56605453+ryandash@users.noreply.github.com>
2022-03-21 19:21:41 -0400

    Remove BoundedBuffer (#164)
    - remove BoundedBuffer.java, BoundedBufferTest.java, and SubsystemMessagePasser.java
    
    - remove all BoundedBuffer stuff from ElevatorSubsystem, FloorSubsystem, and Scheduler
    
    - Fix up some of SchedulerTest (#165)

--- src/test/java/scheduler/SchedulerTest.java
+++ src/test/java/scheduler/SchedulerTest.java
@@ -1,15 +1,23 @@
 package scheduler;
 
+import client_server_host.Port;
+import elevatorsystem.Elevator;
 import elevatorsystem.ElevatorSubsystem;
+import elevatorsystem.MovementState;
 import floorsystem.FloorSubsystem;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import requests.ElevatorMonitor;
+import requests.ElevatorRequest;
 import requests.ServiceRequest;
-import systemwide.BoundedBuffer;
+import requests.SystemEvent;
 import systemwide.Direction;
 import systemwide.Origin;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,93 +30,88 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 class SchedulerTest {
 
-    private BoundedBuffer elevatorBuffer;
-    private BoundedBuffer floorBuffer;
-    private Scheduler scheduler;
-    private ElevatorSubsystem elevatorSubsystem;
-    private FloorSubsystem floorSubsystem;
-    private ServiceRequest serviceRequest;
+    private static Scheduler schedulerClient;
+    private static Scheduler schedulerServer;
+    private static FloorSubsystem floorSubsystem;
+    private static ElevatorSubsystem elevatorSubsystem;
+    private static Elevator elevator1;
+    private static SystemEvent serviceRequest;
+    private static SystemEvent elevatorRequest;
+    private static SystemEvent elevatorMonitor;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    static void setUpOnce() {
         // Request
         serviceRequest = new ServiceRequest(LocalTime.NOON, 1, Direction.UP, Origin.FLOOR_SYSTEM);
-
-        // Set up buffers
-        elevatorBuffer = new BoundedBuffer();
-        floorBuffer = new BoundedBuffer();
+        elevatorRequest = new ElevatorRequest(LocalTime.now(), 1, Direction.UP, 2, Origin.FLOOR_SYSTEM);
+        elevatorMonitor = new ElevatorMonitor(0.0, MovementState.ACTIVE, 1, Direction.UP, 1);
+        elevatorMonitor.setOrigin(Origin.ELEVATOR_SYSTEM);
 
         // Set up systems
-        scheduler = new Scheduler(elevatorBuffer, floorBuffer);
-        elevatorSubsystem = new ElevatorSubsystem(elevatorBuffer);
-        floorSubsystem = new FloorSubsystem(floorBuffer);
+        schedulerClient = new Scheduler(Port.CLIENT_TO_SERVER.getNumber());
+        schedulerServer = new Scheduler(Port.SERVER_TO_CLIENT.getNumber());
+        floorSubsystem = new FloorSubsystem();
+        elevatorSubsystem = new ElevatorSubsystem();
+        elevator1 = new Elevator(1, elevatorSubsystem);
+        elevatorSubsystem.addElevator(elevator1);
+        schedulerClient.addElevatorMonitor(elevator1.getElevatorNumber());
+
+        new Thread(floorSubsystem, floorSubsystem.getClass().getSimpleName()).start();
+        new Thread(schedulerClient, schedulerClient.getClass().getSimpleName()).start();
+        new Thread(schedulerServer, schedulerServer.getClass().getSimpleName()).start();
+        new Thread(elevatorSubsystem, elevatorSubsystem.getClass().getSimpleName()).start();
     }
 
     @AfterEach
     void tearDown() {
+
     }
 
     @Test
     void sendElevatorRequest() {
-        // Send req from scheduler to elevator buffer
-        scheduler.sendMessage(serviceRequest, elevatorBuffer, Origin.SCHEDULER);
-        assertEquals(1, elevatorBuffer.getSize());
+        assertTrue(elevator1.getRequestQueue().isEmpty());
+
+        try {
+            schedulerClient.processData(InetAddress.getLocalHost(), elevatorRequest);
+            Thread.sleep(1000);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
 
         // Elevator receives request from buffer
-        ServiceRequest result = (ServiceRequest) elevatorSubsystem.receiveMessage(elevatorBuffer, Origin.ELEVATOR_SYSTEM);
-        System.out.println(result);
-        assertEquals(0, elevatorBuffer.getSize());
+        assertTrue(!elevator1.getRequestQueue().isCurrentQueueEmpty());
 
         // Verify values
-        assertEquals(LocalTime.NOON, result.getTime());
-        assertEquals(1, result.getFloorNumber());
-        assertEquals(Direction.UP, result.getDirection());
+        assertEquals(1, elevator1.getRequestQueue().removeRequest());
+        assertEquals(2, elevator1.getRequestQueue().removeRequest());
+        assertEquals(Direction.UP, elevator1.getServiceDirection());
     }
 
     @Test
-    void sendFloorRequest() {
+    void sendElevatorMonitor() {
         // Send req from scheduler to FloorBuffer
-        scheduler.sendMessage(serviceRequest, floorBuffer, Origin.SCHEDULER);
-        assertEquals(1, floorBuffer.getSize());
-
-        // Elevator receives request from buffer
-        ServiceRequest result = (ServiceRequest) floorSubsystem.receiveMessage(floorBuffer, Origin.FLOOR_SYSTEM);
-        assertEquals(0, floorBuffer.getSize());
-
-        // Verify values
-        assertEquals(LocalTime.NOON, result.getTime());
-        assertEquals(1, result.getFloorNumber());
-        assertEquals(Direction.UP, result.getDirection());
+        try {
+            schedulerServer.processData(InetAddress.getLocalHost(), elevatorMonitor);
+            Thread.sleep(1000);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        assertEquals(1, floorSubsystem.getRequestSize()); // fails because floorSubsystem processes requests instantly
     }
 
     @Test
     void receiveElevatorRequest() {
-        // Send request to buffer
-        elevatorSubsystem.sendMessage(serviceRequest, elevatorBuffer, Origin.ELEVATOR_SYSTEM);
+        floorSubsystem.addRequest(elevatorRequest);
 
-        // Scheduler receives request from buffer
-        ServiceRequest result = (ServiceRequest) scheduler.receiveMessage(elevatorBuffer, Origin.SCHEDULER);
-
-        // Verify values
-        assertEquals(LocalTime.NOON, result.getTime());
-        assertEquals(1, result.getFloorNumber());
-        assertEquals(Direction.UP, result.getDirection());
     }
 
     @Test
     void receiveFloorRequest() {
-        // Ensure buffer is initially empty
-        assertTrue(floorBuffer.isEmpty());
 
-        // Send request to buffer
-        floorSubsystem.sendMessage(serviceRequest, floorBuffer, Origin.FLOOR_SYSTEM);
-
-        // Scheduler receives request from buffer
-        ServiceRequest result = (ServiceRequest) scheduler.receiveMessage(floorBuffer, Origin.SCHEDULER);
-
-        // Verify values
-        assertEquals(LocalTime.NOON, result.getTime());
-        assertEquals(1, result.getFloorNumber());
-        assertEquals(Direction.UP, result.getDirection());
     }
 }
