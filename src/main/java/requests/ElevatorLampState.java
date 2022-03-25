c27418364d95b131cf0cd5cdd889cc397de4cde4
Ryan Dash <ryandash@cmail.carleton.ca>
2022-03-25 12:31:18 -0400

    - add more javadocs

--- src/main/java/requests/ElevatorLampState.java
+++ src/main/java/requests/ElevatorLampState.java
@@ -1,5 +1,10 @@
 package requests;
 
+/**
+ * ElevatorLampState is an enum representing the possible states of an elevator's lamp.
+ *
+ * @author Liam Tripp
+ */
 public enum ElevatorLampState {
 	ON,
 	OFF
