fc69b6fe7c37e22e7d7b8ff8f1c0e323d5701966
Bnortron <72238857+bnortron@users.noreply.github.com>
2022-03-27 18:34:04 -0400

    Initial commit:
    - Commented out an error line in ElevatorSubsystem
    - Added basic functionality to ArrivalSensor for comparing ApproachEvent and ServiceRequest
    - Changed ApproachEvent to have floorToVisit attribute
    
    Signed-off-by: Brady Norton <bnort.11@gmail.com>

--- src/main/java/floorsystem/ArrivalSensor.java
+++ src/main/java/floorsystem/ArrivalSensor.java
@@ -1,6 +1,9 @@
 package floorsystem;
 
 import requests.ApproachEvent;
+import requests.ServiceRequest;
+
+import java.util.ArrayList;
 
 /**
  * Arrival Sensor checks the floorNumber in ApproachEvent is equal to the Floor's floorNumber
@@ -8,7 +11,10 @@ import requests.ApproachEvent;
  *
  * @author Ramit Mahajan
  */
-public class ArrivalSensor {
+public class ArrivalSensor{
+
+	// Requests for this arrival sensors floor
+	ArrayList<ServiceRequest> activeRequests;
 
 	private final int floorNumber;
 
@@ -19,6 +25,7 @@ public class ArrivalSensor {
 	 */
 	ArrivalSensor(int floorNumber) {
 		this.floorNumber = floorNumber;
+		activeRequests = new ArrayList<>();
 	}
 
 	/**
@@ -30,5 +37,36 @@ public class ArrivalSensor {
 		if (approachEvent.getFloorNumber() == floorNumber) {
 			approachEvent.allowElevatorStop();
 		}
+	}
+
+	/**
+	 * Receive ServiceRequest and add it to the list of activeRequests for this arrival sensor
+	 */
+	public void addRequest(ServiceRequest request){
+		activeRequests.add(request);
+	}
+
+	/**
+	 * Compare the ApproachEvent with the ServiceRequests in activeRequests
+	 *
+	 * @param approachEvent indicates that an elevator is approaching this floor
+	 *
+	 * @return true if the elevator approaching should stop, false if not
+	 */
+	public boolean shouldStop(ApproachEvent approachEvent){
+		if(!activeRequests.isEmpty()){
+			for(int i=0; i<activeRequests.size(); i++){
+				if(approachEvent.getElevatorNumber() == activeRequests.get(i).getElevatorNumber()){
+					if(approachEvent.getDirection() == activeRequests.get(i).getDirection()){
+						if(approachEvent.getFloorNumber() == approachEvent.getFloorToVisit() && approachEvent.getFloorNumber() == activeRequests.get(i).getFloorNumber()){
+							// Should this be called? or just return true/false?
+							approachEvent.allowElevatorStop();
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return false;
 	}
 }
