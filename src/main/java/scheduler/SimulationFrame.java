5ddbb4369b4feb7203958466c41f142797db0816
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-04-05 13:55:57 -0400

    Presenter: added frameSetup method from SimulationFrame

--- src/main/java/scheduler/SimulationFrame.java
+++ src/main/java/scheduler/SimulationFrame.java
@@ -4,9 +4,6 @@ import elevatorsystem.MovementState;
 import requests.ElevatorMonitor;
 import systemwide.Direction;
 
-import javax.swing.*;
-import java.awt.*;
-
 /**
  * SimulatoinFrame demos the GUI of the simulation.
  *
@@ -14,37 +11,14 @@ import java.awt.*;
  */
 public class SimulationFrame {
 
-    private ElevatorViewContainer elevatorViewContainer;
-    private Presenter presenter;
-    private JFrame frame;
-
-    public SimulationFrame() {
-        elevatorViewContainer = new ElevatorViewContainer(20);
-        presenter = new Presenter();
-        presenter.addView(elevatorViewContainer);
-    }
-
-    public void frameSetup() {
-        frame = new JFrame("Elevator Simulation");
-        int height = Toolkit.getDefaultToolkit().getScreenSize().height - Toolkit.getDefaultToolkit().getScreenInsets(new JDialog().getGraphicsConfiguration()).bottom;
-        int width = Toolkit.getDefaultToolkit().getScreenSize().width;
-        frame.setSize(width, height);
-        frame.add(elevatorViewContainer.getPanel());
-
-        frame.setVisible(true);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-    }
-
-    // TODO: this method should be moved to the model and renamed
-    public void testElevatorMonitorUpdate(ElevatorMonitor elevatorMonitor) {
-        presenter.updateElevatorView(elevatorMonitor);
-    }
-
     public static void main(String[] args) {
-        SimulationFrame simulationFrame = new SimulationFrame();
-        simulationFrame.frameSetup();
+        ElevatorViewContainer elevatorViewContainer = new ElevatorViewContainer(20);
+        Presenter presenter = new Presenter();
+        presenter.addView(elevatorViewContainer);
+
+        presenter.frameSetup();
         ElevatorMonitor elevatorMonitor = new ElevatorMonitor(0);
         elevatorMonitor.updateMonitor(new ElevatorMonitor(0, MovementState.ACTIVE, 2, Direction.DOWN, 0, true));
-        simulationFrame.testElevatorMonitorUpdate(elevatorMonitor);
+        presenter.updateElevatorView(elevatorMonitor);
     }
 }
