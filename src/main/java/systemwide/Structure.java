07eab09e3a5eff124c79cb222c8c11390d9b1227
Ryan Dash <ryandash@cmail.carleton.ca>
2022-04-07 21:54:33 -0400

    - remove initializeStructure

--- src/main/java/systemwide/Structure.java
+++ src/main/java/systemwide/Structure.java
@@ -109,42 +109,4 @@ public class Structure implements Serializable {
 	public void setDoorsTime(int time) {
 		doorsTime = time;
 	}
-
-	/**
-	 * Initializes the Structure's properties.
-	 */
-	public void initializeStructure(Presenter presenter) {
-
-		Scheduler schedulerClient = new Scheduler(Port.CLIENT_TO_SERVER.getNumber());
-		Scheduler schedulerServer = new Scheduler(Port.SERVER_TO_CLIENT.getNumber());
-
-		if (presenter != null){
-			schedulerClient.setPresenter(presenter);
-		}
-
-		ElevatorSubsystem elevatorSubsystem = new ElevatorSubsystem();
-		for (int elevatorNumber = 1; elevatorNumber <= numberOfElevators; elevatorNumber++) {
-			schedulerClient.addElevatorMonitor(elevatorNumber);
-		}
-		elevatorSubsystem.initializeElevators(this);
-		elevatorSubsystem.initializeElevatorThreads();
-
-		FloorSubsystem floorSubsystem = new FloorSubsystem();
-		for (int i = 1; i <= numberOfFloors; i++) {
-			Floor floor = new Floor(i, floorSubsystem);
-			floorSubsystem.addFloor(floor);
-		}
-
-		Thread schedulerClientOrigin, schedulerServerOrigin, elevatorSubsystemOrigin, floorSubsystemOrigin;
-
-		schedulerClientOrigin = new Thread(schedulerClient, schedulerClient.getClass().getSimpleName());
-		schedulerServerOrigin = new Thread(schedulerServer, schedulerServer.getClass().getSimpleName());
-		elevatorSubsystemOrigin = new Thread(elevatorSubsystem, elevatorSubsystem.getClass().getSimpleName());
-		floorSubsystemOrigin = new Thread(floorSubsystem, floorSubsystem.getClass().getSimpleName());
-
-		schedulerClientOrigin.start();
-		schedulerServerOrigin.start();
-		elevatorSubsystemOrigin.start();
-		floorSubsystemOrigin.start();
-	}
 }
