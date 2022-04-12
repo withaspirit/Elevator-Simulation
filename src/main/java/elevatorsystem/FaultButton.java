0424b6d7c97fd66f9d9e6066f8b9e61d478c97e4
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-04-12 17:09:02 -0400

    Reformat Code

--- src/main/java/elevatorsystem/FaultButton.java
+++ src/main/java/elevatorsystem/FaultButton.java
@@ -1,41 +1,42 @@
 package elevatorsystem;
 
-import java.awt.Color;
-import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
- *  FaultButton is the button class used to inject door stuck faults.
- * 
+ * FaultButton is the button class used to inject door stuck faults.
+ *
  * @author Julian
- * */
+ */
 public class FaultButton implements ActionListener {
 
-	//For each elevator
-	private JPanel faultPanel;
-	private JToggleButton doorFaultButton;
-	private Elevator elevator;
-	private boolean malfunction;
-	
-	public FaultButton(Elevator elevator) {
-		this.elevator = elevator;
-		this.malfunction = false;
-		int elevatorNumber = elevator.getElevatorNumber();
-		faultPanel = new JPanel();
-		doorFaultButton = new JToggleButton("Door Fault");
-		
+    //For each elevator
+    private JPanel faultPanel;
+    private JToggleButton doorFaultButton;
+    private Elevator elevator;
+    private boolean malfunction;
+
+    public FaultButton(Elevator elevator) {
+        this.elevator = elevator;
+        this.malfunction = false;
+        int elevatorNumber = elevator.getElevatorNumber();
+        faultPanel = new JPanel();
+        doorFaultButton = new JToggleButton("Door Fault");
+
         faultPanel.add(doorFaultButton);
         doorFaultButton.addActionListener(this);
-        
+
         Border border = BorderFactory.createLineBorder(Color.BLACK);
         TitledBorder titledBorder = BorderFactory.createTitledBorder(border, "Elevator " + elevatorNumber);
         titledBorder.setTitleJustification(TitledBorder.CENTER);
         titledBorder.setTitlePosition(TitledBorder.ABOVE_TOP);
         faultPanel.setBorder(titledBorder);
-	}
-	
+    }
+
     /**
      * Returns the JPanel of ElevatorView.
      *
@@ -44,11 +45,11 @@ public class FaultButton implements ActionListener {
     public JPanel getPanel() {
         return faultPanel;
     }
-	
+
     //Overriding actionPerformed() method
     @Override
     public void actionPerformed(ActionEvent e) {
-    	malfunction = !malfunction;
-    	elevator.setDoorsMalfunctioning(malfunction);
+        malfunction = !malfunction;
+        elevator.setDoorsMalfunctioning(malfunction);
     }
 }
--- src/main/java/elevatorsystem/FaultInjectorView.java
+++ src/main/java/elevatorsystem/FaultInjectorView.java
@@ -1,24 +1,24 @@
 package elevatorsystem;
 
-import java.util.ArrayList;
 import javax.swing.*;
+import java.util.ArrayList;
 
 /**
- *  FaultInjectorView is the GUI that helps to inject Door Stuck faults using a push button
- * 
+ * FaultInjectorView is the GUI that helps to inject Door Stuck faults using a push button
+ *
  * @author Julian
  */
 public class FaultInjectorView {
 
-	//For elevator subsystem
+    //For elevator subsystem
     private ArrayList<FaultButton> faultButtons;
     private JPanel buttonListPanel;
     private JPanel containerPanel;
 
-	public FaultInjectorView(ArrayList<Elevator> elevatorList) {
+    public FaultInjectorView(ArrayList<Elevator> elevatorList) {
         int numberOfElevators = elevatorList.size();
         //Initializing the buttons
-		this.faultButtons = new ArrayList<>();
+        faultButtons = new ArrayList<>();
         buttonListPanel = new JPanel();
         for (int i = 0; i < numberOfElevators; i++) {
             faultButtons.add(new FaultButton(elevatorList.get(i)));
@@ -29,10 +29,10 @@ public class FaultInjectorView {
         scrollPane.setBorder(BorderFactory.createEtchedBorder());
         containerPanel = new JPanel();
         containerPanel.add(scrollPane);
-        
+
         JFrame frame = new JFrame("Elevator Simulation");
         frame.add(containerPanel);
         frame.pack();
         frame.setVisible(true);
-	}
+    }
 }
