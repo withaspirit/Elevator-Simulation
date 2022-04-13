43c2b65d25c4dc7fe62026b7001e99fc2d96e947
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-04-12 20:14:52 -0400

    Reformat code

--- src/main/java/elevatorsystem/FaultButton.java
+++ src/main/java/elevatorsystem/FaultButton.java
@@ -14,57 +14,55 @@ import java.awt.event.ActionListener;
  */
 public class FaultButton implements ActionListener {
 
-	//For each elevator
-	private JPanel faultPanel;
-	private JToggleButton doorFaultButton;
-	private Elevator elevator;
-	private String name;
+    private JPanel faultPanel;
+    private JToggleButton doorFaultButton;
+    private Elevator elevator;
+    private String name;
 
-	/**
-	 * FaultButton constructor with elevator reference
-	 *
-	 * @param
-	 * @param
-	 */
-	public FaultButton(Elevator elevator, String name) {
-		this.name = name;
-		this.elevator = elevator;
-		int elevatorNumber = elevator.getElevatorNumber();
-		faultPanel = new JPanel();
-		doorFaultButton = new JToggleButton(name);
+    /**
+     * FaultButton constructor with elevator reference
+     *
+     * @param
+     * @param
+     */
+    public FaultButton(Elevator elevator, String name) {
+        this.name = name;
+        this.elevator = elevator;
+        int elevatorNumber = elevator.getElevatorNumber();
+        faultPanel = new JPanel();
+        doorFaultButton = new JToggleButton(name);
 
-		faultPanel.add(doorFaultButton);
-		doorFaultButton.addActionListener(this);
-		Border border = BorderFactory.createLineBorder(Color.BLACK);
-		TitledBorder titledBorder = BorderFactory.createTitledBorder(border, "Elevator " + elevatorNumber);
-		titledBorder.setTitleJustification(TitledBorder.CENTER);
-		titledBorder.setTitlePosition(TitledBorder.ABOVE_TOP);
-		faultPanel.setBorder(titledBorder);
-	}
+        faultPanel.add(doorFaultButton);
+        doorFaultButton.addActionListener(this);
+        Border border = BorderFactory.createLineBorder(Color.BLACK);
+        TitledBorder titledBorder = BorderFactory.createTitledBorder(border, "Elevator " + elevatorNumber);
+        titledBorder.setTitleJustification(TitledBorder.CENTER);
+        titledBorder.setTitlePosition(TitledBorder.ABOVE_TOP);
+        faultPanel.setBorder(titledBorder);
+    }
 
-	/**
-	 * Returns the JPanel of FaultButton.
-	 *
-	 * @return the FaultButton's JPanel
-	 */
-	public JPanel getPanel() {
-		return faultPanel;
-	}
+    /**
+     * Returns the JPanel of FaultButton.
+     *
+     * @return the FaultButton's JPanel
+     */
+    public JPanel getPanel() {
+        return faultPanel;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (name.equals(Fault.DOOR_STUCK.getName())) {
-			boolean doorIsMalfunctioning = !elevator.doorsAreMalfunctioning();
-			if (doorIsMalfunctioning) {
-				elevator.setDoorsMalfunctioning(true);
-			}
-			doorFaultButton.setSelected(false);
-		} else if (name.equals(Fault.ELEVATOR_STUCK.getName())) {
-			boolean cartIsMalfunctioning = !elevator.cartIsMalfunctioning();
-			if (cartIsMalfunctioning) {
-				elevator.setCartMalfunctioning(true);
-			}
-			doorFaultButton.setSelected(false);
-		}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (name.equals(Fault.DOOR_STUCK.getName())) {
+            boolean doorIsMalfunctioning = !elevator.doorsAreMalfunctioning();
+            if (doorIsMalfunctioning) {
+                elevator.setDoorsMalfunctioning(true);
+            }
+        } else if (name.equals(Fault.ELEVATOR_STUCK.getName())) {
+            boolean cartIsMalfunctioning = !elevator.cartIsMalfunctioning();
+            if (cartIsMalfunctioning) {
+                elevator.setCartMalfunctioning(true);
+            }
+        }
+		doorFaultButton.setSelected(false);
 	}
 }
