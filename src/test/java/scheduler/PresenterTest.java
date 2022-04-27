baa6361508e6a6928bee218065b8a9d15320c0af
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-04-27 11:40:07 -0400

    Fix PresenterTest, Move InputFileReader (#247)
    - InputFIleReader: moved from package "misc" to "systemwide"
    - ElevatorViewContainer: moved (index - 1) from Presenter.updateElevatorView to ElevatorViewContainer.getElevatorView (index), fixing bug in PresenterTest
    - PresenterTest: fixed incorrect elevatorView being accessed. Cleaned up and added testing for currentRequest to testElevatorViewUpdate

--- src/test/java/scheduler/PresenterTest.java
+++ src/test/java/scheduler/PresenterTest.java
@@ -6,73 +6,68 @@ import elevatorsystem.MovementState;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import requests.ElevatorMonitor;
+import requests.ServiceRequest;
 import systemwide.Direction;
+import systemwide.Origin;
 
 import javax.swing.*;
+
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * PresenterTest tests for the Presenter class methods and integration with the system
- * 
- * @author Julian
- * */
+ *
+ * @author Julian, Liam Tripp
+ */
 public class PresenterTest {
 
     private Presenter presenter;
     private ElevatorViewContainer elevatorViewContainer;
+    private static final int NUMBER_OF_ELEVATORS = 20;
 
     @BeforeEach
     void setUp() {
         presenter = new Presenter();
-        elevatorViewContainer = new ElevatorViewContainer(20);
+        elevatorViewContainer = new ElevatorViewContainer(NUMBER_OF_ELEVATORS);
+        presenter.addView(elevatorViewContainer);
     }
 
     @Test
-    void testUpdateElevatorView() {
-        presenter.addView(elevatorViewContainer);
+    void testElevatorViewDefaultStatus() {
+        // TODO: assert the default status of each elevatorView is correct
+    }
 
-        //Updating first view
+    @Test
+    void testElevatorViewUpdate() {
+        // Updating first view
         int elevatorNumber = 1;
         int currentFloor = 2;
         Direction currentDirection = Direction.DOWN;
+        ServiceRequest currentRequest = new ServiceRequest(LocalTime.now(), currentFloor, currentDirection, Origin.SCHEDULER);
+        currentRequest.setElevatorNumber(elevatorNumber);
+
         MovementState movementState = MovementState.ACTIVE;
         Direction movementDirection = Direction.UP;
         Doors.State doorsState = Doors.State.OPEN;
         Fault fault = Fault.NONE;
 
         ElevatorMonitor elevatorMonitor = new ElevatorMonitor(elevatorNumber, currentFloor, currentDirection, movementState, movementDirection, doorsState, fault, false, 0);
+        elevatorMonitor.setCurrentRequest(currentRequest);
         presenter.updateElevatorView(elevatorMonitor);
 
         ElevatorView elevatorView = elevatorViewContainer.getElevatorView(elevatorMonitor.getElevatorNumber());
-        JTextPane[] panes = elevatorView.getStatusPanes();
-        assertEquals(panes[0].getText(), Integer.toString(currentFloor));
-        assertEquals(panes[1].getText(), currentDirection.toString());
-        assertEquals(panes[2].getText(), movementState.getName());
-        assertEquals(panes[3].getText(), movementDirection.getName());
-        assertEquals(panes[4].getText(), doorsState.toString());
-        assertEquals(panes[5].getText(), fault.getName());
+        JTextPane[] statusPanes = elevatorView.getStatusPanes();
 
-
-        //Updating a second view for a different elevator
-        elevatorNumber = 2;
-        currentFloor = 1;
-        currentDirection = Direction.UP;
-        movementState = MovementState.ACTIVE;
-        movementDirection = Direction.DOWN;
-        doorsState = Doors.State.OPEN;
-        fault = Fault.ELEVATOR_STUCK;
-
-        elevatorMonitor = new ElevatorMonitor(elevatorNumber, currentFloor, currentDirection, movementState, movementDirection, doorsState, fault, false, 0);
-        presenter.updateElevatorView(elevatorMonitor);
-
-        elevatorView = elevatorViewContainer.getElevatorView(elevatorMonitor.getElevatorNumber());
-        panes = elevatorView.getStatusPanes();
-        assertEquals(panes[0].getText(), Integer.toString(currentFloor));
-        assertEquals(panes[1].getText(), currentDirection.toString());
-        assertEquals(panes[2].getText(), movementState.getName());
-        assertEquals(panes[3].getText(), movementDirection.getName());
-        assertEquals(panes[4].getText(), doorsState.toString());
-        assertEquals(panes[5].getText(), fault.getName());
+        assertEquals(Integer.toString(currentFloor), statusPanes[0].getText());
+        assertEquals(currentDirection.toString(), statusPanes[1].getText());
+        assertEquals(movementState.getName(), statusPanes[2].getText());
+        assertEquals( movementDirection.getName(), statusPanes[3].getText());
+        assertEquals(doorsState.toString(), statusPanes[4].getText());
+        assertEquals( fault.getName(), statusPanes[5].getText());
+        // exclude time from request.toString()
+        int localTimeCutoffIndex = "HH:mm:ss.SSS ".length();
+        assertEquals(currentRequest.toString().substring(localTimeCutoffIndex), statusPanes[6].getText());
     }
 }
