3e3306e76ac84c4bcf31bda783c4ed945919d2f2
cyberphoria <61635007+cyberphoria@users.noreply.github.com>
2022-04-27 11:16:59 -0400

    PresenterTest: add currentRequest to statusPane, remove redundant code

--- src/test/java/scheduler/PresenterTest.java
+++ src/test/java/scheduler/PresenterTest.java
@@ -6,9 +6,13 @@ import elevatorsystem.MovementState;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import requests.ElevatorMonitor;
+import requests.ServiceRequest;
 import systemwide.Direction;
+import systemwide.Origin;
 
 import javax.swing.*;
+
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -36,12 +40,16 @@ public class PresenterTest {
         int elevatorNumber = 1;
         int currentFloor = 2;
         Direction currentDirection = Direction.DOWN;
+        ServiceRequest currentRequest = new ServiceRequest(LocalTime.now(), currentFloor, currentDirection, Origin.SCHEDULER);
+        currentRequest.setElevatorNumber(elevatorNumber);
+
         MovementState movementState = MovementState.ACTIVE;
         Direction movementDirection = Direction.UP;
         Doors.State doorsState = Doors.State.OPEN;
         Fault fault = Fault.NONE;
 
         ElevatorMonitor elevatorMonitor = new ElevatorMonitor(elevatorNumber, currentFloor, currentDirection, movementState, movementDirection, doorsState, fault, false, 0);
+        elevatorMonitor.setCurrentRequest(currentRequest);
         presenter.updateElevatorView(elevatorMonitor);
 
         ElevatorView elevatorView = elevatorViewContainer.getElevatorView(elevatorMonitor.getElevatorNumber());
@@ -52,27 +60,8 @@ public class PresenterTest {
         assertEquals(panes[3].getText(), movementDirection.getName());
         assertEquals(panes[4].getText(), doorsState.toString());
         assertEquals(panes[5].getText(), fault.getName());
-
-
-        //Updating a second view for a different elevator
-        elevatorNumber = 2;
-        currentFloor = 1;
-        currentDirection = Direction.UP;
-        movementState = MovementState.ACTIVE;
-        movementDirection = Direction.DOWN;
-        doorsState = Doors.State.OPEN;
-        fault = Fault.ELEVATOR_STUCK;
-
-        elevatorMonitor = new ElevatorMonitor(elevatorNumber, currentFloor, currentDirection, movementState, movementDirection, doorsState, fault, false, 0);
-        presenter.updateElevatorView(elevatorMonitor);
-
-        elevatorView = elevatorViewContainer.getElevatorView(elevatorMonitor.getElevatorNumber());
-        panes = elevatorView.getStatusPanes();
-        assertEquals(panes[0].getText(), Integer.toString(currentFloor));
-        assertEquals(panes[1].getText(), currentDirection.toString());
-        assertEquals(panes[2].getText(), movementState.getName());
-        assertEquals(panes[3].getText(), movementDirection.getName());
-        assertEquals(panes[4].getText(), doorsState.toString());
-        assertEquals(panes[5].getText(), fault.getName());
+        // exclude time from request.toString()
+        int localTimeCutoffIndex = "HH:mm:ss.SSS ".length();
+        assertEquals(panes[6].getText(), currentRequest.toString().substring(localTimeCutoffIndex));
     }
 }
